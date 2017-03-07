f1a340ab5aa265dd937c09afefd31f7882142a93
Kevin <aeriello@gmail.com>
2017-03-06 20:29:06 -0800

    Almost done with Menu changes in state and cursor changes on screen - have a few holes so calls to the handle_menu_change and update_menu_buffer methods are commented out for now

--- StartupMainLoop.asm
+++ StartupMainLoop.asm
@@ -43,7 +43,7 @@ start_battle:
         ld bc, 2048
         ldir
         ld hl, bordered_third_attr_buf
-        ld de, $5A00
+        ld de, menu_third_attr_buf
         ld bc, 256
         ldir
 
@@ -88,14 +88,58 @@ start_battle:
         ldir
 
                                                                 ; randomly choose two characters for player 2 char A and B
-                                                                ; fill up player 2 char A stats buffer with character dictionary + 3rd char select buffer
-                                                                ; fill up player 2 char B stats buffer with character dictionary + 4th char select buffer
+
+        ;;; fill up player 2 char A stats buffer with character dictionary + 3rd char select buffer
+        ld hl, char_select_p2_c1
+        ld a, (hl)                                              ; try to have char_index to be [0,5] (6 and 7 results in undefined behavior)
+        and 7
+        ld (hl), a                                              ; multiply by 6 to get the offset from the character dictionary into a
+        rlca
+        rlca
+        add a, (hl)
+        add a, (hl)
+        ld hl, char_data                                        ; add the offset to char_data and put in hl the new address (= char_data + (6*char_index))
+        ld b, 0
+        ld c, a
+        add hl, bc
+        push hl                                                 ; calculate the address of the second data structure holding real-time player data
+        ld hl, in_battle_chars
+        ld bc, 16
+        add hl, bc
+        ld d, h
+        ld e, l
+        pop hl                                                 ; copy the data from the dictionary char_data into the data structure holding real-time player data
+        ld bc, 6
+        ldir
+
+        ;;; fill up player 2 char B stats buffer with character dictionary + 4th char select buffer
+        ld hl, char_select_p2_c2
+        ld a, (hl)                                              ; try to have char_index to be [0,5] (6 and 7 results in undefined behavior)
+        and 7
+        ld (hl), a                                              ; multiply by 6 to get the offset from the character dictionary into a
+        rlca
+        rlca
+        add a, (hl)
+        add a, (hl)
+        ld hl, char_data                                        ; add the offset to char_data and put in hl the new address (= char_data + (6*char_index))
+        ld b, 0
+        ld c, a
+        add hl, bc
+        push hl                                                 ; calculate the address of the second data structure holding real-time player data
+        ld hl, in_battle_chars
+        ld bc, 24
+        add hl, bc
+        ld d, h
+        ld e, l
+        pop hl                                                 ; copy the data from the dictionary char_data into the data structure holding real-time player data
+        ld bc, 6
+        ldir
 
                                                                 ; initialize game states in game state buffer
 
                                                                 ; read char stat buffers and draw into actual display in the data (top) third (note: only call this again after a call into the animation/actionResolve section)
 
-                                                                ; read menu state and draw into screen buffer of menu (bottom) third (only call this again after an interpretation of input)
+        ;call update_menu_buffer                                 ; read menu state and draw into screen buffer of menu (bottom) third (only call this again after an interpretation of input)
         
 main_loop:
                                                                 ; jump if animateBool is true (to the animation/actionResolve section)
@@ -108,12 +152,19 @@ main_loop:
         ld de, $4800
         ld bc, 2048
         ldir
-        ; NOTE: fill in part to change attributes for visual third
+        ld hl, visual_third_attr_buf
+        ld de, $5900
+        ld bc, 256
+        ldir
+
         ld hl, menu_third_px_buf
-        ;;ld de, $5000
+        ld de, $5000
         ld bc, 2048
         ldir
-        ; NOTE: fill in part to change attributes for menu third
+        ld hl, menu_third_attr_buf
+        ld de, $5A00
+        ld bc, 256
+        ldir
 
         jp main_loop                                            ; re-call main_loop
 
@@ -176,7 +227,7 @@ check_input_char_select:
 ;;; called by input handler after system indicates that it has accepted the moves the player has made
 ;;; will first randomly do moves for the enemy characters
 ;;; then will loop to calculate the state of the current attack and then call the animation loop
-;;; animation loop should ret and then the continue this loop for the next attack
+;;; after the animation loop returns, update the data third of the screen, then continue this loop for the next attack
 ;;; after all 4 attacks have resolved-animated-resolved-animated, we jump back into main_loop
 action_resolve:
         ret
@@ -265,7 +316,7 @@ handle_input_load_read_key:
 handle_input_end_read:                                          ; a holds the pressed key
         ld hl, last_input
         cp (hl)
-        jr z, handle_input_unchanged
+        jp z, handle_input_unchanged
         cp 'A'
         jr z, handle_input_a
         cp 'S'
@@ -281,49 +332,220 @@ handle_input_end_read:                                          ; a holds the pr
         ;;cp ' '
         ;;jr z, handle_input_none
 handle_input_none:
-        ld de, $3D00
-        ld hl, last_input
+        ld hl, last_input                                       ; record currently read key as last_input
         ld (hl), a
+        ld de, $3D00
         jp handle_input_print_char
 handle_input_a:
-        call short_beep
-        ld de, $3E08
-        ld hl, last_input
+        ld hl, last_input                                       ; record currently read key as last_input
         ld (hl), a
+        ld a, 0                                                 ; change menu/cursor
+        ;call handle_menu_change
+        call short_beep                                         ; beep once
+        ld de, $3E08
         jp handle_input_print_char
 handle_input_s:
-        call short_beep
-        ld de, $3E98
-        ld hl, last_input
+        ld hl, last_input                                       ; record currently read key as last_input
         ld (hl), a
+        ld a, 1                                                 ; change menu/cursor
+        ;call handle_menu_change
+        call short_beep                                         ; beep once
+        ld de, $3E98
         jp handle_input_print_char
 handle_input_d:
-        call short_beep
-        ld de, $3E20
-        ld hl, last_input
+        ld hl, last_input                                       ; record currently read key as last_input
         ld (hl), a
+        ld a, 0                                                 ; change menu/cursor
+        ;call handle_menu_change
+        call short_beep                                         ; beep once
+        ld de, $3E20
         jp handle_input_print_char
 handle_input_w:
-        call short_beep
-        ld de, $3EB8
-        ld hl, last_input
+        ld hl, last_input                                       ; record currently read key as last_input
         ld (hl), a
+        ld a, 1                                                 ; change menu/cursor
+        ;call handle_menu_change
+        call short_beep                                         ; beep once
+        ld de, $3EB8
         jp handle_input_print_char
 handle_input_enter:
-        call short_beep
-        ld de, $3F28
-        ld hl, last_input
+        ld hl, last_input                                       ; record currently read key as last_input
         ld (hl), a
+        ld a, 2                                                 ; change menu/cursor
+        ;call handle_menu_change
+        call short_beep                                         ; beep once
+        ld de, $3F28
         jp handle_input_print_char
 handle_input_shift:
-        ld de, $3F98
-        ld hl, last_input
+        ld hl, last_input                                       ; record currently read key as last_input
         ld (hl), a
+        ld a, 3                                                 ; change menu/cursor
+        ;call handle_menu_change
+        call short_beep                                         ; beep once
+        ld de, $3F98
         jp handle_input_print_char
 handle_input_print_char:
         ld hl, $0045
         call print_char_to_menu
 handle_input_unchanged:
+        ret
+
+handle_menu_change:
+        push af                                                 ; save register states
+        push hl
+        push bc
+        push de
+        cp 0                                                    ; switch case
+        jr z, handle_menu_change_a_d
+        cp 1
+        jr z, handle_menu_change_w_s
+        cp 2
+        jr z, handle_menu_change_enter
+        cp 3
+        jr z, handle_menu_change_shift
+        jp handle_menu_change_end
+handle_menu_change_a_d:
+        ld hl, menu_state_var1                                  ; update the cursor: if on the left (cursor bits either 0000 or 0010) then change to right (0001 or 0011) and vice versa
+        ld a, (hl)
+        xor $01
+        ld (hl), a
+        ;call update_menu_buffer                                 ; update menu visuals
+        jp handle_menu_change_end
+handle_menu_change_w_s:
+        ld hl, menu_state_var1                                  ; update the cursor: if on the top (cursor bits either 0000 or 0001) then change to bottom (0010 or 0011) and vice versa
+        ld a, (hl)
+        xor $02
+        ld (hl), a
+        ;call update_menu_buffer                                 ; update menu visuals
+        jp handle_menu_change_end
+handle_menu_change_enter:
+        ld hl, menu_state_var1                                  ; check menu and cursor and check if action is valid
+        ld a, (hl)                                              ; check cursor position; save in b
+        and $03
+        ld b, a
+        ld a, (hl)                                              ; check num choices
+        and $0C
+        rrca
+        rrca
+        cp b                                                    ; if cursor is greater than num choices (i.e. if a < b or this compare results in the c flag set)
+        jr c, handle_menu_change_end                            ; then do nothing, this is an invalid enter input
+
+
+
+        ;call update_menu_buffer                                 ; update menu visuals
+        jp handle_menu_change_end
+handle_menu_change_shift:
+
+
+
+        ;call update_menu_buffer                                 ; update menu visuals
+handle_menu_change_end:
+        pop de                                                  ; no matter what, restore register states
+        pop bc
+        pop hl
+        pop af
+        ret
+
+update_menu_buffer:
+        push hl                                                 ; save registers
+        push af
+        push bc
+        push de
+        ld hl, bordered_third_attr_buf                          ; clear any changed attributes
+        ld de, menu_third_attr_buf
+        ld bc, 256
+        ldir
+        ld b, 0                                                 ; clear all text entries in the menu
+        ld hl, $0042
+update_menu_buffer_clear_preloop:
+        ld c, 10                                                ; b indicates which quadrant of the menu, c is the number of characters to clear out
+update_menu_buffer_clear_loop:
+        ld de, $3D00
+        push hl
+        push bc
+        call print_char_to_menu
+        pop bc
+        pop hl
+        dec c
+        inc hl
+        ld a, c                                                 ; loop back for 10 characters
+        cp 0
+        jr nz, update_menu_buffer_clear_loop
+        inc b
+        ld a, b                                                 ; loop for all 4 quadrants
+        cp 4                                                    ; if quadrant counter b is >= 4, then end the clear_loop
+        jr nc, update_menu_buffer_clear_end
+        cp 1
+        jr z, update_menu_buffer_clear_2nd_quad
+        cp 2
+        jr z, update_menu_buffer_clear_3rd_quad
+        cp 3
+        jr z, update_menu_buffer_clear_4th_quad
+update_menu_buffer_clear_4th_quad:
+        ld hl, $00D1
+        jp update_menu_buffer_clear_preloop
+update_menu_buffer_clear_2nd_quad:
+        ld hl, $0051
+        jp update_menu_buffer_clear_preloop
+update_menu_buffer_clear_3rd_quad:
+        ld hl, $00C2
+        jp update_menu_buffer_clear_preloop
+update_menu_buffer_clear_end:
+        ld hl, in_battle_chars                                  ; populate the menu's text entries based on the current characters' turn and the current menu state
+        
+
+
+
+        ld hl, menu_state_var1                                  ; update the attributes based on the cursor position
+        ld a, (hl)
+        and $03
+        ld de, menu_third_attr_buf
+        cp 0
+        jr z, update_menu_buffer_attr0
+        cp 1
+        jr z, update_menu_buffer_attr1
+        cp 2
+        jr z, update_menu_buffer_attr2
+update_menu_buffer_attr4:
+        ld hl, $80
+        push hl
+        add hl, de
+        ld a, (hl)
+        or $80
+        ld (hl), a
+        pop hl
+        jp update_menu_buffer_attr_end
+update_menu_buffer_attr0:
+        ld hl, $00
+        push hl
+        add hl, de
+        ld a, (hl)
+        or $80
+        ld (hl), a
+        pop hl
+        jp update_menu_buffer_attr_end
+update_menu_buffer_attr1:
+        ld hl, $10
+        push hl
+        add hl, de
+        ld a, (hl)
+        or $80
+        ld (hl), a
+        pop hl
+        jp update_menu_buffer_attr_end
+update_menu_buffer_attr2:
+        ld hl, $90
+        push hl
+        add hl, de
+        ld a, (hl)
+        or $80
+        ld (hl), a
+        pop hl
+update_menu_buffer_attr_end:
+        pop de                                                  ; restore registers
+        pop bc
+        pop af
+        pop hl
         ret
 
 
@@ -366,11 +588,11 @@ char_select_var:
 char_select_p1_c1:
         defb $00
 char_select_p1_c2:
-        defb $00
+        defb $01
 char_select_p2_c1:
-        defb $00
+        defb $02
 char_select_p2_c2:
-        defb $00
+        defb $03
 
 ;;; buffer to keep track of the input (or lack thereof) of the last frame
 last_input:
@@ -380,7 +602,7 @@ last_input:
 menu_state_char_turn:
         defb $00                                                ; 0 = p1_c1, 1 = p1_c2, 2 = p2_c1, 3 = p2_c2
 menu_state_var1:
-        defb $00                                                ; 4 most sig bits = which menu (by index); 4 least sig bits = cursor
+        defb $0C                                                ; 4 most sig bits = which menu (by index); 2 least sig bits = cursor position; middle 2 bits = number of choices on the current menu
 
 
 ;;; Char Dictionary (6 bytes x 6 characters): < HP, Armor, MR, move1 offset, move2 offset, move3 offset >
@@ -415,7 +637,7 @@ move_dictionary:
 	defb $04, $06, $00 ,$ff, $68, $78, $b0, $28, $78, $88, $ff, $ff, $ff
 	defb $01, $01, $03 ,$ff, $68, $78, $b0, $28, $78, $90, $ff, $ff, $ff
 	
-;;; (12 bytes x 4 characters) < HP, Armor, MR, move1 offset, move2 offset, move3 offset. Queued Move, Queued Target>
+;;; (8 bytes x 4 characters) < HP, Armor, MR, move1 offset, move2 offset, move3 offset. Queued Move, Queued Target>
 in_battle_chars:
 	defb $00, $00, $00, $00, $00, $00, $00, $00
 	defb $00, $00, $00, $00, $00, $00, $00, $00
